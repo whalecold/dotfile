1f1727bf906fc45df8180793bec4f235d3eba6fd
whalecold <zls1129@gmail.com>
2023-02-04 02:40:54 +0800

    add install step

--- install.sh
+++ install.sh
@@ -1,31 +1,53 @@
 #!/bin/bash
 
-#brew install coreutils
 #mkdir ~/.dircolors
 #git clone https://github.com/gibbling666/dircolors.git ~/.dircolors
 #
-#brew install go@1.17
-#brew insatll go
-#brew install neovim
-#brew install node
-#brew install tmux
-#brew install global
-#brew install trash
-#brew install python3
-##brew install watchman
-#brew install pyenv
-#brew install wget
-#brew install ag
-#brew install rg
 #
-#
+
+function install_soft_for_mac() {
+	brew install coreutils
+	brew install go@1.17
+	brew insatll go
+	brew install neovim
+	brew install node
+	brew install tmux
+	brew install global
+	brew install trash
+	brew install python3
+	#brew install watchman
+	brew install pyenv
+	brew install wget
+	brew install ag
+	brew install rg
+	brew install shellcheck
+	brew install istioctl
+	brew install skopeo
+}
+
+arch=$(uname)
+
+if [ "$arch" == "Darwin" ]; then
+	install_soft_for_mac
+fi
+
 
 root_path=$HOME
 config_root=$root_path/.config
-current_path=`pwd`
+current_path=$(pwd)
 
-files=(nvim alacritty)
-for file in ${files[@]}
+# create dir if not exist.
+if [ ! -d  "$config_root" ]; then
+	echo "$config_root not exist, initing..."
+	mkdir -p "$config_root"
+fi
+
+
+dirs=(nvim alacritty)
+for dir in "${dirs[@]}"
 do
-	echo "ln -s $current_path/$file $config_root/$file"
+	if [ -d "$config_root"/"$dir" ]; then 
+		rm -rf "${config_root:?}"/"$dir"
+	fi
+	ln -s "${current_path}"/"${dir}" "${config_root}"/"$dir"
 done
